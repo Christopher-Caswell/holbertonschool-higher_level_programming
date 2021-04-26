fe5705066ae0c2e704afb9a756c22e44cb8993d9
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-26 17:13:28 +0000

    Init repo, 0, 1, 2

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# write a python script that prints a thing
+
+print('"Programming is like building a multilingual puzzle')
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+> This directory is the first step into Python in accordance with the Holberton Curriculum.
+>> Only a flesh wound
