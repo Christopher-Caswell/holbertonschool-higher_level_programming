4c9f40a6647e59c45cda3ebdc1eab1153ae7e6f5
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-28 18:15:25 +0000

    Exclusion

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for x in range(len(my_list)):
+        if idx > len(my_list) or idx < 0:
+            return None
         if idx == x:
             print("{:d}".format(int(x)))
