c89651c48b4349314dd876e0fa5643ea9e26bb71
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-28 18:24:19 +0000

    Maths

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for x in range(len(my_list)):
-        if idx > len(my_list) or idx < 0:
-            return None
-        if idx == x:
-            print("{:d}".format(int(x)))
+    if idx >= len(my_list) or idx < 0:
+        return None
+    print("{:d}".format(int(idx)))
