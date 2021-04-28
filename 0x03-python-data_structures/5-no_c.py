2d5efd40e34c781139c249b2f592b80669aa9922
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-28 20:45:43 +0000

    upd8s

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        return (my_string.translate({ord(x): None for x in 'Cc'}))
+    return (my_string.translate({ord(x): None for x in 'Cc'}))
