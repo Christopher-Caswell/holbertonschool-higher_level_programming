b5d9b7ad6e44eaae38ed85fc5829a1f4bfe78599
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-28 04:58:41 +0000

    Init

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == '__main__':
-    for x in range(len(dir(hidden_4))):
+    for x in dir(hidden_4):
         if x[0] == '_' and x[1] == '_':
             continue
         else:
