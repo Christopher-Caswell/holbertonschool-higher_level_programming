2bf1c9eb024abd8f03a8c794d5309a8a17f565af
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-29 04:39:03 +0000

    Null != 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     x = tuple()
-    if len(sentence) is None:
+    if len(sentence) is 0:
         x = (0, None)
     else:
         x = (len(sentence), sentence[0])
