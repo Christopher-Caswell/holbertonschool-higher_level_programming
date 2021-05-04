3b8611ad6a87dea8ba99dc032790d1cbd397b8e0
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-04 05:15:41 +0000

    Forgot to update a file on shell

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in my_list:
-            if 0 < x < 6:
-                print('{}'.format(my_list[:x]), end="")
+        for i in range(x):
+                print('{}'.format(my_list[i]), end="")
+                count += 1
         print('')
+        return count
     except:
-        print('{}'.format(my_list[x]))
-    finally:
+        print('')
         return count
