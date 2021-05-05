5f00292a0442857af4020b99247b40e3fd86256e
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-05 00:18:27 +0000

    Magix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-
 """Module is a bytecode replication puzzle."""
 
 
 import math
+
 
 class MagicClass():
     """A circle, mathematically"""
 
     def __init__(self, radius=0):
         """what is the radius of our circle"""
+
         self.radius = 0
         """initiated lines 0-6"""
+
         if type(radius) is not int and type(radius) is not float:
             """9 - 42"""
+
             raise TypeError("radius must be a number")
         self.radius = radius
 
     def area(self):
         """radius squared times pi, intuitively"""
+
         return((self.radius ** 2) * math.pi)
 
     def circumference(self):
         """radius times pi times 2, intuitively"""
+
         return((2 * math.pi * self.radius))
