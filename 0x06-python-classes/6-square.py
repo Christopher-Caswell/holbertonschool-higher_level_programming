7fb15c6306ac2a821fac31833ca41a292ba423a6
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-05 05:07:57 +0000

    Class struggle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,13 +50,13 @@ class Square:
     def position(self, value):
         """The setting of the variable: position"""
         if not isinstance(value, tuple):
-            return TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int:
-            return TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[1]) is not int:
-            return TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if not len(value) < 2:
-            return TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
-            return TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.position = value
