e0ee396cad61e521a4861b7bf221956794dc2d0b
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-05 00:23:50 +0000

    Doc

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-
-"""Module, bro. Module. This module defines a private square."""
+"""This module defines a private square."""
 
 
 class Square:
     """Show me the private Square"""
+
     def __init__(self, size=0):
         """A protection before the computation"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         """The actual calculation"""
+
         else:
             if size < 0:
                 raise ValueError("size must be >= 0")
             else:
                 """Lastly, declare the value to define the square"""
+
                 self.__size = size
