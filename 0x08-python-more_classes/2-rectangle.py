7f41f35c76f6ce6076c56305c1eb40da7f14bee6
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-07 21:14:38 +0000

    .

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -13,43 +13,68 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.height = height
         self.width = width
 
-        @property
-        def height(self):
-            """Returns height"""
-            return self.__height
+    @property
+    def height(self):
+        """Returns height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """Sets height"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            value = self.__height
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-        @property
-        def width(self):
-            """Returns width"""
-            return self.__width
+    @property
+    def width(self):
+        """Returns width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Sets width"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """"Sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        def area(self):
-            """defines the area of the rectangle"""
-            return self.width * self.height
+    def area(self):
+        """defines the area of the rectangle"""
+        return self.width * self.height
 
-        def perimeter(self):
-            """returns the perimeter of the rectangle"""
-            if self.width is 0 or self.height is 0:
-                return 0
-            return (self.width * 2) + (self.height * 2)
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        z = ""
+        # g = 0
+        # if g != (self.height) and g != 0:
+        #     z += chr(10)
+        for x in range(self.height):
+            for y in range(self.width):
+                z += "#"
+            if x != self.height - 1:
+                z += chr(10)
+        # z += chr(10)
+        return z
+
+    def __repr__(self):
+        """The rectangle outputs a string of itself"""
+        return "Rectangle({}, {})".format(self.width, self.height)
