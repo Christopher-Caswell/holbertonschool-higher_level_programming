1e6eec58e8154956dc9f415b198d62bca5761555
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-07 05:37:21 +0000

    .

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print()
+        print("", end ="")
     for i in range(size):
         for i in range(size):
             print("#", end="")
