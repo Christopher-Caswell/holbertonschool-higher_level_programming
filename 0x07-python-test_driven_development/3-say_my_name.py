ebd5bc87aab77fbf5ebbdf32fce2d77f038b0524
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-07 05:15:39 +0000

    .

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,12 +5,13 @@ first and last names must be strings
 Last name will return as empty input
 """
 
+
 def say_my_name(first_name, last_name=""):
     """Step one: make the func"""
     if not isinstance(first_name, str):
-        return TypeError("first_name must be a string")
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        return TypeError("last_name must be a string")
+        raise TypeError("last_name must be a string")
         """Can't stop the func"""
 
     print("My name is {} {}".format(first_name, last_name))
