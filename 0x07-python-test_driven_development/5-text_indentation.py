f6650a42890d386d45de91ec9e9e5cb4dcf8240f
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-09 21:05:43 +0000

    Reup 5/6

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,28 +8,41 @@ raise a flag if not a string, though
 
 def text_indentation(text):
     """Consider me noted"""
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     """
     chr(10) is a new line.
     Written in ASCII to keep clean
     and somewhat save space for pep8
+    x is a string variable used to-
+    -convert the old string into new
     """
-    # r = ""
+    i = 0
     x = ""
-    for i in range(len(text) - 1):
-        if text[i] is chr(46):
-            """chr(46) = '.'"""
+    while i in range(len(text) - 1):
+        if text[i] is '.':
             x += text[i] + chr(10) + chr(10)
-        elif text[i] is chr(63):
-            """chr(63) = '?'"""
+
+        elif text[i] is '?':
             x += text[i] + chr(10) + chr(10)
-        elif text[i] is chr(58):
-            """chr(58) = ':'"""
+
+        elif text[i] is ':':
             x += text[i] + chr(10) + chr(10)
-        else:
-            if (text[i - 1] is not chr(46)) and\
-                    (text[i - 1] is not chr(63)) and\
-                    (text[i - 1] is not chr(58)):
+
+        elif (text[i - 1] is '.' or
+              (text[i - 1] is ':') or
+              (text[i - 1] is '?') and
+              text[i] is " "):
+
+            while text[i] == " ":
+                i += 1
+            x += text[i]
+        elif (text[i - 1] is not '.' and
+              (text[i - 1] is not ':') and
+              (text[i - 1] is not '?')):
                 x += text[i]
-    print("{}{}".format(x, text[-1]))
+        i += 1
+
+    print("{}{}".format(x, text[-1]), end="")
