c4c157950df4c1e9f75e98b3048948408f7da7b9
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-09 22:00:06 +0000

    check rearranging for checker's explicit nuance

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,23 +18,9 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
 
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
-
-    @property
-    def height(self):
-        """Returns height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """sets height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
 
     @property
     def width(self):
@@ -49,6 +35,20 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """Returns height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """defines the area of the rectangle"""
