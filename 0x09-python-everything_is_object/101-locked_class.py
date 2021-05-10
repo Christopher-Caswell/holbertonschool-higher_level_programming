132bb552ba41f2b9507377fd04c8f0adb3b6cf13
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-10 19:54:30 +0000

    Why not disassemble it

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,8 +8,4 @@ variable is first_name
 
 class LockedClass():
     """A class for the class of it"""
-    __slots = ["first.name"]
-
-    def __init__(self, first_name):
-        """oof"""
-        self.first.name = first_name
+    __slots = "first.name"
