db35b862f3468840bf815701df9b1aa3d6556ef0
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-10 19:51:49 +0000

    .

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,9 +5,11 @@ wherein the only malleable
 variable is first_name
 """
 
+
 class LockedClass():
     """A class for the class of it"""
     __slots = ["first.name"]
-        def __init__(self, first_name):
-            """oof"""
-            self.first.name = first_name
+
+    def __init__(self, first_name):
+        """oof"""
+        self.first.name = first_name
