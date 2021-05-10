0e33e55ff96ac56bce70388fc978bac6cea3c5b8
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-10 19:48:32 +0000

    .

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
 """
 Create a locked class
-Locked class is to protect the books
-Dont go stealing dictionaries
+wherein the only malleable
+variable is first_name
 """
-
 
 class LockedClass():
     """A class for the class of it"""
-
-    __slots = ["first.name"]
+    __slots__ = ["first.name"]
+        def __init__(self, first_name):
+            """oof"""
+            self.first.name = first_name
