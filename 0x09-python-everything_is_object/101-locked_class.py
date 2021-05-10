94c5c8fa2d32c10fb2897de08acef355ff06196d
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-10 18:42:17 +0000

    Guessing I'm right?

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Create a locked class
+wherein the only malleable
+variable is first_name
+"""
+
+class LockedClass:
+    """A class for the class of it"""
+    __slots__ = ["first.name"]
