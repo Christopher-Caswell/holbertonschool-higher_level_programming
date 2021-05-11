fd149c91c28e429d25db13846f794dab06c58b3b
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-11 19:45:50 +0000

    Init 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,3 +25,16 @@ class BaseGeometry:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Child class made of geometry
+    """
+
+    def __init__(self, width, height):
+        """The legendary block code of aaaaargh"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
