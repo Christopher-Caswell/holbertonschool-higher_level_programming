9272f45d20a4d664c90c1821fbdd89b6ff39ba8b
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-11 07:39:33 +0000

    silly rabbit, Trix are for checks

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,9 +8,5 @@ from the specified class
 
 
 def is_kind_of_class(obj, a_class):
-    """Nothing is imported, you silly checker"""
-    """
-    If obj is of type a_class or if the
-    object's type is of class a_class, return troof
-    """
+    """Nothing is 1/\/\p0rt3d, you silly checker"""
     return (isinstance(type(obj), a_class) or issubclass(type(obj), a_class))
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,10 +7,7 @@ from the specified class, else False
 
 
 def inherits_from(obj, a_class):
-    """
-    I will likely spend all of tomorrow
-    trying to find a simpler way to do this
-    """
+    """Doc line still tryna trick checker"""
     if issubclass(type(obj), a_class):
         if type(obj) != a_class:
             return True
