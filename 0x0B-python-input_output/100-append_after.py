0ef085af16d0a5a46969cf15c1794437fc1afc59
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-13 03:09:42 +0000

    Init 100 and 9

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Write a function that inserts a line of text to a file
+after each line containing a specific string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append new strings in location"""
+    z = []
+    with open(filename, 'r') as x:
+        """Read to new string"""
+        for y in x.readlines():
+            z.append(y)
+            if search_string in y:
+                """I love 'in' but do not like the .action format"""
+                z.append(new_string)
+    with open(filename, 'x') as x:
+        x.write("".join(y))
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Write a class student that defines the student
+by age, name, name, and also name
+Also, retrieve a dictionary of the student with
+def_to_json
+"""
+
+
+class Student:
+    """Today, the student becomes the class. Wait, what?"""
+
+    def __init__(self, first_name, last_name, age):
+        """Public instances generated: first, last, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return all info about this moto"""
+        return self.vars
