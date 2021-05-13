78020de57c3107f05a9902bdcd44a0c7df672153
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-13 02:29:07 +0000

    Init 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+!/usr/bin/python3
+"""
+Write a function that returns the
+dictionary of a class
+"""
+
+
+def class_to_json(obj):
+    """Welp, there's a function for everything in Python"""
+    return vars(obj)
