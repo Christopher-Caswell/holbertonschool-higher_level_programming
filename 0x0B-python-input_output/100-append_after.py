96168aed568350bb1e82e3023a16d3779e6244f2
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-13 03:21:45 +0000

    Simpler readability

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,10 +10,10 @@ def append_after(filename="", search_string="", new_string=""):
     z = []
     with open(filename, 'r+') as x:
         """Read to new string"""
-        for y in x.readlines():
+        for eachline in x:
             z.append(y)
-            if search_string in y:
+            if search_string in eachline:
                 """I love 'in' but do not like the .action format"""
                 z.append(new_string)
-    with open(filename, 'w') as x:
-        x.write("".join(y))
+    with open(filename, 'w+') as a:
+        a.write("".join(y))
