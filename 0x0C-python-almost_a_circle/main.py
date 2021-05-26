eee9f0fa54e0f499af015546a803554b7bec9f06
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-26 20:23:07 +0000

    .gitignore update. very explicit

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,101 +1,28 @@
 #!/usr/bin/python3
-""" 10-main """
-from models.square import Square
-
-if __name__ == "__main__":
-
-    s1 = Square(5)
-    print(s1)
-    print(s1.size)
-    s1.size = 10
-    print(s1)
-
-    try:
-        s1.size = "9"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-""" 11-main """
-from models.square import Square
-
-if __name__ == "__main__":
-
-    s1 = Square(5)
-    print(s1)
-
-    s1.update(10)
-    print(s1)
-
-    s1.update(1, 2)
-    print(s1)
-
-    s1.update(1, 2, 3)
-    print(s1)
-
-    s1.update(1, 2, 3, 4)
-    print(s1)
-
-    s1.update(x=12)
-    print(s1)
-
-    s1.update(size=7, y=1)
-    print(s1)
-
-    s1.update(size=7, id=89, y=1)
-    print(s1)
-
-""" 12-main """
+""" 15-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2, 1, 9)
-    print(r1)
-    r1_dictionary = r1.to_dictionary()
-    print(r1_dictionary)
-    print(type(r1_dictionary))
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    Rectangle.save_to_file([r1, r2])
 
-    r2 = Rectangle(1, 1)
-    print(r2)
-    r2.update(**r1_dictionary)
-    print(r2)
-    print(r1 == r2)
-""" 9-main """
-from models.square import Square
+    with open("Rectangle.json", "r") as file:
+        print(file.read())
+print("\n\n\n")
 
-print("9")
+""" 16-main """
+from models.rectangle import Rectangle
+
 if __name__ == "__main__":
 
-    s1 = Square(5)
-    print(s1)
-    print(s1.area())
-    s1.display()
-
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
-print()
-
-""" Check """
-from models.square import Square
-
-s = Square(5, 1, 4, 3)
-s.update(id=89, y=2)
-
-if s.id != 89:
-    print("ID of the Square must be updated to 89: {}".format(s.id))
-    exit(1)
-    
-print("OK", end="")
-
-print()
+    list_input = [
+        {'id': 89, 'width': 10, 'height': 4}, 
+        {'id': 7, 'width': 1, 'height': 7}
+    ]
+    json_list_input = Rectangle.to_json_string(list_input)
+    list_output = Rectangle.from_json_string(json_list_input)
+    print("[{}] {}".format(type(list_input), list_input))
+    print("[{}] {}".format(type(json_list_input), json_list_input))
+    print("[{}] {}".format(type(list_output), list_output))
