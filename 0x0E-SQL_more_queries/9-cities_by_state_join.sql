12df0169aa4ec37ded7d7c2f8a9a95a09fb4880e
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-05-28 03:54:34 +0000

    Referential repair

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,4 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT cities.id, cities.name, states.name
 FROM cities JOIN states ON cities.state_id = states.id
-ORDER BY hbtn_0d_usa.cities.id ASC;
+ORDER BY cities.id ASC;
